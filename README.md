789c5c2fada92c906739fb8da7e1d132748969c9
Mike Cunneen <mike@cunneen.biz>
2014-04-18 11:38:56 +0800

    Initial meteorite package wrapper around json-editor

---
+++ README.md
@@ -0,0 +1,1 @@
+A straightforward wrapper around the excellent [json-editor](https://github.com/jdorn/json-editor), a component that renders JSON documents as forms, and collects form data as JSON.
---
+++ package.js
@@ -0,0 +1,6 @@
+Package.describe({
+  summary: "A JSON Editor and Form Generator"
+});
+Package.on_use(function (api, where) {
+  api.add_files('json-editor/dist/jsoneditor.min.js', 'client');
+});
